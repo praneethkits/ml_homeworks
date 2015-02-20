7f2a4f252d1b4f330dc68b1f794e64870a7dce77
praneeth <pvallem@tetrationanalytics.com>
2015-02-20 07:20:51 -0600

    Logistic regression is completed.

--- hw2/bayes.py
+++ hw2/bayes.py
@@ -3,31 +3,7 @@ import argparse
 import glob
 import os, sys
 import math
-
-
-def read_file_contents(file_name=None):
-    """This function is used to read the file contents and return contents
-    in a string format.
-    Args:
-        file_name: file which need to be read.
-    Returns:
-        SUCCESS when successfully read else FAILURE
-        file_content: Complete content of file.
-    Raises:
-        NA.
-    """
-    if file_name is None:
-        return False, 'NA'
-
-    try:
-        file_descriptor = open(file_name, "rb")
-    except IOError:
-        logging.error("Unable to open given protobuf file")
-        return False, 'NA'
-
-    file_content = file_descriptor.read()
-    file_descriptor.close()
-    return True, file_content
+from general_functions import read_file_contents
 
 
 class NaiveBayes(object):
